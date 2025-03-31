40ae7ad8269a4b18b017276bc34d6e2479fb4294
umranterece <chost609@gmail.com>
2025-03-31 03:49:27 +0300

    #

---
+++ Project.DAL/Repositories/Abstracts/IRepository.cs
@@ -0,0 +1,42 @@
+﻿using Project.ENTITIES.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Repositories.Abstracts
+{
+    public interface IRepository<T> where T : class,IEntity
+    {
+        // List Commands
+        IQueryable<T> GetAll();
+        IQueryable<T> GetActives();
+        IQueryable<T> GetModifieds();
+        IQueryable<T> GetPassives();
+
+        //Modify Commands
+        void Add(T item);
+        Task AddAsync(T item);
+        void AddRange(List<T> list);
+        Task AddRangeAsync(List<T> list);
+        Task Update(T item);
+        Task UpdateRange(List<T> list);
+
+        void Delete(T item);
+        void DeleteRange(List<T> list);
+        void Destroy(T item); // Todo: RemoveAsync varsa Taske cevirelim
+        void DestroyRange(List<T> list); // Todo: RemoveAsync varsa Taske cevirelim
+
+        //LINQ Commands
+        IQueryable<T> Where(Expression<Func<T, bool>> exp);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> exp);
+        Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> exp);
+        IQueryable<X> Select<X>(Expression<Func<T, X>> exp);
+
+        //Find
+        Task<T> FindAsync(int id);
+
+    }
+}
---
+++ Project.DAL/Repositories/Concrete/OrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using Project.DAL.Context;
+using Project.DAL.Repositories.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Repositories.Concrete
+{
+    public class OrderRepository:BaseRepository<Order>,IOrderRepository
+    {
+        public OrderRepository(MyContext db):base(db)
+        {
+            
+        }
+    }
+}
